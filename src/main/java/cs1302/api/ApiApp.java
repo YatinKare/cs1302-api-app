94e9540507495c16dad890028e0318a605a963bc
Yatin Kare <yatin.kare@gmail.com>
2023-11-29 02:13:35 -0500

    Set up testing variables for compilation

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -2,20 +2,39 @@ package cs1302.api;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
  * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
  */
 public class ApiApp extends Application {
+    /** Instance variables for the stage and scene. */
     Stage stage;
     Scene scene;
+    
+    /** The root container for the application scene graph. */
+
     VBox root;
+
+    /** The container for the Search functions*/
+    HBox searchBarHBox;
+    TextField searchField;
+    Button searchButton;
+
+    
+    /** The container for the current weather */
+    Text currentWeatherText;
+    
+    /** The container for the DisplayComponent */
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
